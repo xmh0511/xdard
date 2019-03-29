4d77342c2df4892a34497a36558e736c4a913e98
xmh0511 <970252187@qq.com>
2019-03-29 14:10:02 +0800

    update

--- memory/main.cpp
+++ memory/main.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
 #include "memory.hpp"
-class Test
+class Base
+{
+public:
+	int base_v = 10;
+};
+class Test:public Base
 {
 public:
 	Test() = default;
 	Test(int a)
 	{
 		std::cout << "a = " << a << "construct " << std::endl;
+		base_v = a;
 	}
 	~Test()
 	{
 		std::cout << "destory" << std::endl;
 	}
-	int a = 1024;
+	int a = 1;
 };
 int main()
 {
-	//auto ptr = xmh::make_unique<Test>();
-	//xmh::unique_ptr<Test> ptr2;
-	//ptr2 = std::move(ptr);
-	//std::cout << ptr.get() << std::endl;
-	//std::cout << ptr2->a << std::endl;
-	//std::cout << (*ptr2).a << std::endl;
-	xmh::shared_ptr<Test> ptr0 = xmh::make_shared<Test>(1024);
+	auto ptr = xmh::make_unique<Test>();
+	xmh::unique_ptr<Test> ptr2;
+	ptr2 = std::move(ptr);
+	std::cout << ptr.get() << std::endl;
+	std::cout << ptr2->a << std::endl;
+	std::cout << (*ptr2).a << std::endl;
+
+	xmh::shared_ptr<Base> ptr0 = xmh::make_shared<Test>(1024);
+	xmh::shared_ptr<Test> ptr1;
 	{
 		xmh::shared_ptr<Test> ptr(new Test());
-		ptr0 = ptr;
+		ptr1 = ptr;
 	}
+	std::cout << ptr0->base_v << std::endl;
 	std::cin.get();
 	return 0;
 }
--- memory/memory.hpp
+++ memory/memory.hpp
@@ -1,16 +1,17 @@
 #pragma once
+#include <atomic>
 namespace xmh {
-    /* unique_ptr implement begin */
+	/* unique_ptr implement begin */
 	template<typename T>
 	class unique_ptr
 	{
 	public:
-		unique_ptr():raw_pointer_(nullptr)
+		unique_ptr() :raw_pointer_(nullptr)
 		{
 
 		}
 		unique_ptr(T* ptr) :raw_pointer_(ptr) {}
-		unique_ptr(unique_ptr&& uptr):raw_pointer_(uptr.raw_pointer_)
+		unique_ptr(unique_ptr&& uptr) :raw_pointer_(uptr.raw_pointer_)
 		{
 			uptr.raw_pointer_ = nullptr;
 		}
@@ -50,24 +51,35 @@ namespace xmh {
 		T* raw_pointer_;
 	};
 
-	template<typename T,typename...Args>
+	template<typename T, typename...Args>
 	unique_ptr<T> make_unique(Args&&...args)
 	{
 		return unique_ptr<T>(new T{ std::forward<Args>(args)... });
 	}
 	/* unique_ptr implement end */
 
-	template<typename T>
-	class pointer_count
+	/* shared_ptr implement start */
+
+	class pointer_count_implement
 	{
 	public:
-		pointer_count(T* raw_ptr) :raw_pointer_(raw_ptr)
+		virtual ~pointer_count_implement() {}
+	public:
+		virtual void destory() = 0;
+		virtual void share() = 0;
+	protected:
+		//unsigned long long ref_counts_ = 1;
+		std::atomic<std::size_t> ref_counts_ = 1;//thread safe;
+	};
+
+	template<typename T, typename Deleter>
+	class pointer_count :public pointer_count_implement
+	{
+	public:
+		template<typename Callable>
+		pointer_count(T* raw_ptr, Callable&& deleter) :deleter_(std::forward<Callable>(deleter)), raw_pointer_(raw_ptr)
 		{
 
-		}
-		T* get()
-		{
-			return raw_pointer_;
 		}
 		void share()
 		{
@@ -78,58 +90,150 @@ namespace xmh {
 		{
 			if ((--ref_counts_) == 0) {
 				if (raw_pointer_ != nullptr) {
-					delete raw_pointer_;
+					//delete raw_pointer_;
+					deleter_(raw_pointer_);
 					raw_pointer_ = nullptr;
 				}
 				delete this;
 			}
 		}
 	private:
+		Deleter deleter_;
 		T* raw_pointer_ = nullptr;
-		unsigned long long ref_counts_ = 1;
 	};
+
+	template<typename Type>
+	class resource_manager
+	{
+		template<typename Class>
+		friend class resource_manager;
+	public:
+		resource_manager() = default;
+		resource_manager(Type* ptr) :raw_pointer_(ptr)
+		{
+			auto default_deleter = [](auto ptr) {
+				delete ptr;
+			};
+			ref_counter_ = new pointer_count<Type, std::remove_reference_t<decltype(default_deleter)>>(ptr, default_deleter);
+		}
+		template<typename Callable>
+		resource_manager(Type* ptr, Callable&& deleter) :raw_pointer_(ptr)
+		{
+			ref_counter_ = new pointer_count<Type, std::remove_reference_t<Callable>>(ptr, std::forward<Callable>(deleter));
+		}
+		resource_manager(resource_manager const& rm) :raw_pointer_(rm.raw_pointer_), ref_counter_(rm.ref_counter_)
+		{
+			ref_counter_->share();
+		}
+		template<typename Derived, typename U = std::enable_if_t<(std::is_base_of_v<Type, Derived> || std::is_same_v<Type, Derived>)>>
+		resource_manager(resource_manager<Derived> const& rm) : raw_pointer_(rm.raw_pointer_), ref_counter_(rm.ref_counter_)
+		{
+			ref_counter_->share();
+		}
+		resource_manager& operator = (resource_manager const& rm)
+		{
+			if (ref_counter_ != nullptr) {
+				ref_counter_->destory();
+			}
+			raw_pointer_ = rm.raw_pointer_;
+			ref_counter_ = rm.ref_counter_;
+			ref_counter_->share();
+			return *this;
+		}
+		template<typename Derived, typename U = std::enable_if_t<(std::is_base_of_v<Type, Derived> || std::is_same_v<Type, Derived>)>>
+		resource_manager& operator=(resource_manager<Derived> const& rm)
+		{
+			if (ref_counter_ != nullptr) {
+				ref_counter_->destory();
+			}
+			raw_pointer_ = rm.raw_pointer_;
+			ref_counter_ = rm.ref_counter_;
+			ref_counter_->share();
+			return *this;
+		}
+	public:
+		operator bool()
+		{
+			if (ref_counter_ == nullptr) {
+				return false;
+			}
+			return true;
+		}
+	public:
+		Type* get() {
+			return raw_pointer_;
+		}
+	public:
+		~resource_manager() {
+			if (ref_counter_ != nullptr)
+			ref_counter_->destory();
+		}
+	private:
+		Type* raw_pointer_ = nullptr;
+		pointer_count_implement* ref_counter_ = nullptr;
+	};
+	template<typename T>
+	class shared_ptr;
 	template<typename T>
 	class shared_ptr
 	{
+		template<typename Class>
+		friend class shared_ptr;
 	public:
 		shared_ptr() = default;
-		shared_ptr(T* sptr):pointer_ref_(new pointer_count<T>(sptr))
+
+		template<typename Type>
+		shared_ptr(Type* sptr) :pointer_ref_(sptr)
 		{
-             
 		}
-		shared_ptr(shared_ptr const& sptr):pointer_ref_(sptr.pointer_ref_)
+		shared_ptr(shared_ptr const& sptr) :pointer_ref_(sptr.pointer_ref_)
 		{
-			pointer_ref_->share();
+			/*pointer_ref_->share();*/
 		}
+		template<typename Derived>
+		shared_ptr(shared_ptr<Derived> const& sptr) : pointer_ref_(sptr.pointer_ref_)
+		{
+
+		}
+
 		shared_ptr& operator=(shared_ptr const& sptr)
 		{
-			if (pointer_ref_ != nullptr) {
-				pointer_ref_->destory();
-			}
 			pointer_ref_ = sptr.pointer_ref_;
-			pointer_ref_->share();
 			return *this;
 		}
+		template<typename Derived>
+		shared_ptr& operator=(shared_ptr<Derived> const& sptr)
+		{
+			pointer_ref_ = sptr.pointer_ref_;
+			return *this;
+		}
+
 		T* operator ->()
 		{
-			return pointer_ref_->get();
+			return pointer_ref_.get();
 		}
 		T& operator*()
 		{
-			return *(pointer_ref_->get());
+			return *(pointer_ref_.get());
+		}
+	public:
+		operator bool()
+		{
+			return bool(pointer_ref_);
 		}
 	public:
 		~shared_ptr()
 		{
-			pointer_ref_->destory();
+			/*pointer_ref_->destory();*/
 		}
 	private:
-		pointer_count<T>* pointer_ref_ = nullptr;
+		resource_manager<T> pointer_ref_;
 	};
 
-	template<typename T,typename...Args>
+	template<typename T, typename...Args>
 	shared_ptr<T> make_shared(Args&&...args)
 	{
 		return shared_ptr<T>(new T{ std::forward<Args>(args)... });
 	}
+	/* shared_ptr implement end */
 }
