d6bc6c6bc3b60962a82caeef2e1ec253fc10c119
xmh0511 <970252187@qq.com>
2019-09-23 17:59:53 +0800

    update

--- tuple/tuple.hpp
+++ tuple/tuple.hpp
@@ -1,20 +1,52 @@
 #pragma once
 namespace xmh {
 	template<typename...Args>
+	class tuple;
+	template<std::size_t N, std::size_t Max, typename T>
+	struct downtuple {
+
+	};
+
+	template<std::size_t N, std::size_t Max, typename T, typename...Args>
+	struct downtuple<N, Max, tuple<T, Args...>> {
+		using type = typename downtuple<N + 1, Max, tuple<Args...>>::type;
+	};
+
+	template<std::size_t Max, typename T, typename...Args>
+	struct downtuple<Max, Max, tuple<T, Args...>> {
+		using type = tuple<T, Args...>;
+	};
+
+	template<typename T, typename U>
+	struct downtuple_type {};
+
+	template<typename T, typename U, typename...Args>
+	struct downtuple_type<T, tuple<U, Args...>> {
+		using type = typename downtuple_type<T, tuple<Args...>>::type;
+	};
+	template<typename T, typename...Args>
+	struct downtuple_type<T, tuple<T, Args...>> {
+		using type = tuple<T, Args...>;
+	};
+
+	template<typename...Args>
 	class tuple {};
+
 
 	template<typename T, typename...Args>
 	class tuple<T, Args...> :public tuple<Args...> {
 		template<std::size_t N, typename...Parameters>
-		friend auto& get(tuple<Parameters...>&);
+		friend typename downtuple<0, N, tuple<Parameters...>>::type::type& get(tuple<Parameters...>&);
 		template<std::size_t N, typename...Parameters>
-		friend auto& get(tuple<Parameters...>&&);
+		friend typename downtuple<0, N, tuple<Parameters...>>::type::type& get(tuple<Parameters...>&&);
 
 		template<typename P0, typename...P1>
-		friend auto& get(tuple<P1...>&);
+		friend typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&);
 
 		template<typename P0, typename...P1>
-		friend auto& get(tuple<P1...>&&);
+		friend typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&&);
+	public:
+		using type = T;
 	public:
 		tuple<T, Args...>() = default;
 		template<typename P0, typename...Params>
@@ -45,53 +77,26 @@ namespace xmh {
 		}
 	};
 
-	template<std::size_t N, std::size_t Max, typename T>
-	struct downtuple {
-
-	};
-
-	template<std::size_t N, std::size_t Max, typename T, typename...Args>
-	struct downtuple<N, Max, tuple<T, Args...>> {
-		using type = typename downtuple<N + 1, Max, tuple<Args...>>::type;
-	};
-
-	template<std::size_t Max, typename T, typename...Args>
-	struct downtuple<Max, Max, tuple<T, Args...>> {
-		using type = tuple<T, Args...>;
-	};
-
-	template<typename T, typename U>
-	struct downtuple_type {};
-
-	template<typename T, typename U, typename...Args>
-	struct downtuple_type<T, tuple<U, Args...>> {
-		using type = typename downtuple_type<T, tuple<Args...>>::type;
-	};
-	template<typename T, typename...Args>
-	struct downtuple_type<T, tuple<T, Args...>> {
-		using type = tuple<T, Args...>;
-	};
-
 
 	template<std::size_t N, typename...Args>
-	auto& get(tuple<Args...>& v) {
+	typename downtuple<0, N, tuple<Args...>>::type::type& get(tuple<Args...>& v) {
 		using type = typename downtuple<0, N, tuple<Args...>>::type;
 		return (static_cast<type&>(v)).value_;
 	}
 
 	template<std::size_t N, typename...Args>
-	auto& get(tuple<Args...>&& v) {
+	typename downtuple<0, N, tuple<Args...>>::type::type& get(tuple<Args...>&& v) {
 		return get(v);
 	}
 
 	template<typename P0, typename...P1>
-	auto& get(tuple<P1...>& v) {
+	typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>& v) {
 		using type = typename downtuple_type<P0, tuple<P1...>>::type;
 		return (static_cast<type&>(v)).value_;
 	}
 
 	template<typename P0, typename...P1>
-	auto& get(tuple<P1...>&& v) {
+	typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&& v) {
 		return get<P0>(v);
 	}
 }
