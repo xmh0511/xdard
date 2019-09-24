2dbda697e54d8a6882d29816e96b62c01c4f0f83
xmh0511 <970252187@qq.com>
2019-09-24 10:11:46 +0800

    update tuple

--- tuple/tuple.hpp
+++ tuple/tuple.hpp
@@ -1,31 +1,51 @@
 #pragma once
+#include <type_traits>
 namespace xmh {
 	template<typename...Args>
 	class tuple;
-	template<std::size_t N, std::size_t Max, typename T>
-	struct downtuple {
+
+	template<std::size_t N, typename T>
+	struct tuple_transform {
 
 	};
 
-	template<std::size_t N, std::size_t Max, typename T, typename...Args>
-	struct downtuple<N, Max, tuple<T, Args...>> {
-		using type = typename downtuple<N + 1, Max, tuple<Args...>>::type;
+	template<std::size_t N, typename T, typename...Args>
+	struct tuple_transform<N, tuple<T, Args...>> {
+		using type = typename tuple_transform<N - 1, tuple<Args...>>::type;
 	};
 
-	template<std::size_t Max, typename T, typename...Args>
-	struct downtuple<Max, Max, tuple<T, Args...>> {
+	template<typename T, typename...Args>
+	struct tuple_transform<0, tuple<T, Args...>> {
 		using type = tuple<T, Args...>;
 	};
 
+	template<std::size_t N, typename T>
+	struct tuple_element {
+
+	};
+
+	template<std::size_t N, typename T, typename...Args>
+	struct tuple_element<N, tuple<T, Args...>> {
+		using type = typename tuple_element<N - 1, tuple<Args...>>::type;
+	};
+
+	template<typename T, typename...Args>
+	struct tuple_element<0, tuple<T, Args...>> {
+		using type = T;
+	};
+
+	template<std::size_t N, typename T>
+	using tuple_element_t = typename tuple_element<N, T>::type;
+
 	template<typename T, typename U>
-	struct downtuple_type {};
+	struct tuple_transform_ {};
 
 	template<typename T, typename U, typename...Args>
-	struct downtuple_type<T, tuple<U, Args...>> {
-		using type = typename downtuple_type<T, tuple<Args...>>::type;
+	struct tuple_transform_<T, tuple<U, Args...>> {
+		using type = typename tuple_transform_<T, tuple<Args...>>::type;
 	};
 	template<typename T, typename...Args>
-	struct downtuple_type<T, tuple<T, Args...>> {
+	struct tuple_transform_<T, tuple<T, Args...>> {
 		using type = tuple<T, Args...>;
 	};
 
@@ -36,19 +56,19 @@ namespace xmh {
 	template<typename T, typename...Args>
 	class tuple<T, Args...> :public tuple<Args...> {
 		template<std::size_t N, typename...Parameters>
-		friend typename downtuple<0, N, tuple<Parameters...>>::type::type& get(tuple<Parameters...>&);
+		friend constexpr typename tuple_element<N, tuple<Parameters...>>::type& get(tuple<Parameters...>&);
 		template<std::size_t N, typename...Parameters>
-		friend typename downtuple<0, N, tuple<Parameters...>>::type::type& get(tuple<Parameters...>&&);
+		friend constexpr typename tuple_element<N, tuple<Parameters...>>::type& get(tuple<Parameters...>&&);
 
 		template<typename P0, typename...P1>
-		friend typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&);
+		friend constexpr typename tuple_transform_<P0, tuple<P1...>>::type::type& get(tuple<P1...>&);
 
 		template<typename P0, typename...P1>
-		friend typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&&);
+		friend constexpr typename tuple_transform_<P0, tuple<P1...>>::type::type& get(tuple<P1...>&&);
 	public:
 		using type = T;
 	public:
-		tuple<T, Args...>() = default;
+		constexpr tuple<T, Args...>() = default;
 		template<typename P0, typename...Params>
 		tuple<T, Args...>(P0&& p0, Params&& ...args) : tuple<Args...>(std::forward<Params>(args)...), value_(std::forward<P0>(p0)) {
 
@@ -69,34 +89,30 @@ namespace xmh {
 	template<>
 	class tuple<> {
 	public:
-		tuple() = default;
-		tuple(tuple const&) = default;
-		template<typename...Args>
-		tuple(Args&& ...) {
-
-		}
+		constexpr tuple() = default;
+		constexpr tuple(tuple const&) = default;
 	};
 
 
 	template<std::size_t N, typename...Args>
-	typename downtuple<0, N, tuple<Args...>>::type::type& get(tuple<Args...>& v) {
-		using type = typename downtuple<0, N, tuple<Args...>>::type;
+	constexpr typename tuple_element<N, tuple<Args...>>::type& get(tuple<Args...>& v) {
+		using type = typename tuple_transform<N, tuple<Args...>>::type;
 		return (static_cast<type&>(v)).value_;
 	}
 
 	template<std::size_t N, typename...Args>
-	typename downtuple<0, N, tuple<Args...>>::type::type& get(tuple<Args...>&& v) {
+	constexpr typename tuple_element <N, tuple<Args...>>::type& get(tuple<Args...>&& v) {
 		return get(v);
 	}
 
 	template<typename P0, typename...P1>
-	typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>& v) {
-		using type = typename downtuple_type<P0, tuple<P1...>>::type;
+	constexpr typename tuple_transform_<P0, tuple<P1...>>::type::type& get(tuple<P1...>& v) {
+		using type = typename tuple_transform_<P0, tuple<P1...>>::type;
 		return (static_cast<type&>(v)).value_;
 	}
 
 	template<typename P0, typename...P1>
-	typename downtuple_type<P0, tuple<P1...>>::type::type& get(tuple<P1...>&& v) {
+	constexpr typename tuple_transform_<P0, tuple<P1...>>::type::type& get(tuple<P1...>&& v) {
 		return get<P0>(v);
 	}
 }
